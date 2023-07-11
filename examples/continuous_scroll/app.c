890e06fc18d8ce39bebeca6e0a854cf83c1ec352
Manne Tallmarken <manne@systeera.com>
2023-07-11 15:20:26 +0200

    Implemented a few different examples to show usage

---
+++ examples/continuous_scroll/app.c
@@ -0,0 +1,66 @@
+#include "common.h"
+#include "randlib.h"
+#include "cinterplot.h"
+
+#ifndef randf
+#define randf() ((double) rand () / ((double) RAND_MAX+1))
+#endif
+
+int user_main (int argc, char **argv, CinterState *cs)
+{
+    randlib_init (0);
+
+    cinterplot_continuous_scroll_enable (cs);
+    const uint32_t nRows = 2;
+    const uint32_t nCols = 3;
+    const uint32_t n = nRows * nCols;
+    uint32_t bordered = 1;
+    uint32_t margin = 4;
+
+    char *colorSchemes[6] =
+    {
+        "red orange white",
+        "purple blue white",
+        "blue yellow white",
+        "brown turquoise white",
+        "violet indigo white",
+        "chocolate brown red yellow"
+    };
+
+    if (make_sub_windows (cs, nRows, nCols, bordered, margin) < 0)
+        return 1;
+
+    CinterGraph *sineGraph[n];
+    char plotType[6] = {'p','l','s','p','l','s'};
+    for (int i=0; i<n; i++)
+    {
+        sineGraph[i] = graph_new (1000000, 1);
+        graph_attach (cs, sineGraph[i], (uint32_t) i, plotType[i], colorSchemes[i % 6], 8);
+    }
+
+    double v = 0;
+    uint64_t t = 0;
+    double a[n] = {0};
+    while (cinterplot_is_running (cs))
+    {
+        usleep (0);
+
+        for (int i=0; i<n; i++)
+        {
+            v += (1.0 / 1024.0) * 1.3;
+            if (v > 5)
+                v -= 5;
+
+            double f = 0.99;
+            double *r = heavy_tail_ncube (n);
+            a[i] = f * a[i] + (1-f) * r[i] * 0.1;
+            double y = a[i] * sin (2 * M_PI * v * a[i] * (i+1));
+            double x = t;
+            graph_add_point (sineGraph[i], x, y);
+        }
+        t++;
+        cinterplot_redraw_async (cs);
+    }
+
+    return 0;
+}
--- examples/twister/app.c
+++ examples/twister/app.c
@@ -7,6 +7,16 @@
 #define randf() ((double) rand () / ((double) RAND_MAX+1))
 #endif
 
+#define N 1000000
+
+static const uint32_t nRows = 2;
+static const uint32_t nCols = 3;
+static const uint32_t n = nRows * nCols;
+static uint32_t bordered = 1;
+static uint32_t margin = 4;
+static CinterGraph *sineGraph[n];
+static CinterState *cs = NULL;
+
 int on_press (void *twisterDev, int encoder, int pressed)
 {
     print_debug ("encoder: %d, pressed: %d", encoder, pressed);
@@ -21,8 +31,19 @@ int on_button (int button, int pressed)
 
 int on_encoder (void *twisterDev, int encoder, int dir)
 {
+    static double speed = 1.0;
+    double f = (dir > 0) ? 1.01 : 1.0 / 1.01;
+    speed *= f;
+
+    graph_remove_points (sineGraph[0]);
+    for (int i=0; i<N; i++)
+    {
+        double x = i * 1e-3;
+        double y = sin (x * 2 * M_PI * speed);
+        graph_add_point (sineGraph[0], x, y);
+    }
     print_debug ("en: %d dir: %d", encoder, dir);
-    return 0;
+    return 1;
 }
 
 int twister_poll (void *twisterDev)
@@ -71,64 +92,40 @@ int twister_poll (void *twisterDev)
     return 0;
 }
 
-int user_main (int argc, char **argv, CinterState *cs)
+int user_main (int argc, char **argv, CinterState *_cs)
 {
+    cs = _cs;
     randlib_init (0);
-    //void *twisterDev = NULL;
-    //twisterDev = midi_init ("Midi Fighter Twister");
-    //midi_connect (twisterDev);
-
-    //cinterplot_continuous_scroll_enable (cs);
-    const uint32_t nRows = 2;
-    const uint32_t nCols = 3;
-    const uint32_t n = nRows * nCols;
-    uint32_t bordered = 1;
-    uint32_t margin = 4;
+    void *twisterDev = NULL;
+    twisterDev = midi_init ("Midi Fighter Twister");
 
     char *colorSchemes[6] =
     {
-        "red orange white",
-        "purple blue white",
-        "blue yellow white",
-        "brown turquoise white",
-        "violet indigo white",
-        "chocolate brown red yellow"
+        "white blue black",
+        "white",
+        "white",
+        "white",
+        "white",
+        "white",
     };
 
     if (make_sub_windows (cs, nRows, nCols, bordered, margin) < 0)
         return 1;
 
-    CinterGraph *sineGraph[n];
     char plotType[6] = {'p','l','s','p','l','s'};
     for (int i=0; i<n; i++)
     {
-        sineGraph[i] = graph_new (1000000, 1);
-        graph_attach (cs, sineGraph[i], (uint32_t) i, plotType[i], colorSchemes[i % 6]);
+        sineGraph[i] = graph_new (N, 1);
+        graph_attach (cs, sineGraph[i], (uint32_t) i, plotType[i], colorSchemes[i % 6], 4);
     }
 
-    double v = 0;
-    uint64_t t = 0;
-    double a[n] = {0};
     while (cinterplot_is_running (cs))
     {
-        usleep (0);
-
-        for (int i=0; i<n; i++)
-        {
-            v += (1.0 / 1024.0) * 1.3;
-            if (v > 5)
-                v -= 5;
-
-            double f = 0.99;
-            double *r = heavy_tail_ncube (n);
-            a[i] = f * a[i] + (1-f) * r[i] * 0.1;
-            double y = a[i] * sin (2 * M_PI * v * a[i] * (i+1));
-            double x = a[i] * cos (2 * M_PI * v * a[i] * (i+1));
-            //double x = t;
-            graph_add_point (sineGraph[i], x, y);
-        }
-        t++;
-        cinterplot_redraw_async (cs);
+        midi_connect (twisterDev);
+        if (twister_poll (twisterDev))
+            cinterplot_redraw_async (cs);
+        else
+            usleep (1000);
     }
 
     return 0;
