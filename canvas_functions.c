32493209a44a128ddba8a79e13e9cb663487c53b
Manne Tallmarken <manne@systeera.com>
2026-08-22 12:23:16 +0200

    Add staircase line plotting, cleanup comments

--- canvas_functions.c
+++ canvas_functions.c
@@ -242,6 +242,52 @@ static void canvas_fun_2d_line (WorldTransform *world, void *_points, size_t len
     }
 }
 
+static void canvas_fun_2d_stair (WorldTransform *world, void *_points, size_t len, uint32_t logMode, CipCanvas *canvas)
+{
+    BENCHMARK_ADD_CHECKPOINT ("2d_line");
+    double (*restrict points)[2] = _points;
+
+    const int w = canvas->w;
+    const int h = canvas->h;
+
+    int lastXi = -1;
+    int lastYi = -1;
+
+    WORLD_TRANSFORM_DATAPOS_TO_BIN_HOT_LOOP_INIT (world);
+
+    for (size_t i=0; i<len; i++)
+    {
+        double pt[3] = {points[i][0], points[i][1], 0};
+
+        if (logMode & 1) pt[0] = LOGFUN (pt[0]);
+        if (logMode & 2) pt[1] = LOGFUN (pt[1]);
+        if (isnan (pt[0]) || isnan (pt[1]) || isinf (pt[0]) || isinf (pt[1]))
+            continue;
+
+        WORLD_TRANSFORM_DATAPOS_TO_BIN_HOT_LOOP_COMPUTE (pt, xi, yi, wzVal);
+
+        if ((unsigned)xi < (unsigned)w && (unsigned)yi < (unsigned)h)
+        {
+            if (lastXi >= 0)
+            {
+                int xi0 = lastXi;
+                int yi0 = lastYi;
+                int xi1 = xi;
+                int yi1 = yi;
+                cip_canvas_line (canvas, xi0, yi0, xi1, yi0, wzVal);
+                cip_canvas_line (canvas, xi1, yi0, xi1, yi1, wzVal);
+            }
+            lastXi = xi;
+            lastYi = yi;
+        }
+        else
+        {
+            lastXi = -1;
+            lastYi = -1;
+        }
+    }
+}
+
 static void canvas_fun_2d_thick_line (WorldTransform *world, void *_points, size_t len, uint32_t logMode, CipCanvas *canvas)
 {
     BENCHMARK_ADD_CHECKPOINT ("2d_line");
@@ -345,112 +391,43 @@ static void canvas_fun_3d_line (WorldTransform *world, void *_points, size_t len
     }
 }
 
-// every point is one pixel
-//for (int i=0; i<len; i++)
-//{
-//    int xi, yi;
-//    double wzVal;
-
-//    double x[3];
-//    double *point = (double *) (buf + sz * i);
-//    x[0] = point[0];
-//    x[1] = point[1];
-//    x[2] = (dim > 2) ? point[2] : 0;
-
-//    if (logMode & 1) x[0] = LOGFUN (x[0]);
-//    if (logMode & 2) x[1] = LOGFUN (x[1]);
-//    if (isnan (x[0]) || isnan (x[1]) || isinf (x[0]) || isinf (x[1]))
-//        continue;
-
-//    // FIXME: apply log and permute indices here, make support for n-dimensional vectors
-
-//    if (world_transform_datapos_to_bin (world, x, w, h, & xi, & yi, & wzVal) == 0)
+//    // waterfall
+//    if (i0 < 0)
 //    {
-//        int xi0 = lastXi;
-//        int yi0 = lastYi;
-//        int xi1 = xi;
-//        int yi1 = yi;
-//        lastXi = xi;
-//        lastYi = yi;
-
-//        //else if (plotType == 't')
-//        //{
-//        //    // thick line
-//        //    cip_canvas_line (canvas, xi0, yi0, xi1, yi1);
-//        //    cip_canvas_line (canvas, xi0+1, yi0, xi1+1, yi1);
-//        //    cip_canvas_line (canvas, xi0-1, yi0, xi1-1, yi1);
-//        //    cip_canvas_line (canvas, xi0, yi0+1, xi1, yi1+1);
-//        //    cip_canvas_line (canvas, xi0, yi0-1, xi1, yi1-1);
-//        //}
-//        //else if (plotType == 's')
-//        //{
-//        //    // staircase
-//        //    cip_canvas_line (canvas, xi0, yi0, xi1, yi0);
-//        //    cip_canvas_line (canvas, xi1, yi0, xi1, yi1);
-//        //}
-//        //else if (plotType == 'w')
-//        //{
-//        //    // waterfall
-//        //    if (i0 < 0)
-//        //    {
-//        //        print_warning ("truncating");
-//        //        i0 = 0;
-//        //    }
-//        //    if (isnan (xy[0]) || isnan (xy[1]))
-//        //    {
-//        //        // flush row
-//        //        for (uint32_t yi=h-1; yi>0; yi--)
-//        //            for (uint32_t xi=0; xi<w; xi++)
-//        //                bins[yi*w + xi] = bins[(yi-1) * w + xi];
-
-//        //        // construct new row
-//        //        int lastNonZeroXi = -1;
-//        //        for (uint32_t xi=0; xi<w; xi++)
-//        //        {
-//        //            if (canvas->counts[xi] > 1e-5)
-//        //            {
-//        //                double avg = sums[xi] / counts[xi];
-//        //                double s = canvas->world.scaleMtx[1][1];
-//        //                double ymin = canvas->world.centerPos[1] - s;
-//        //                double ymax = canvas->world.centerPos[1] + s;
-//        //                double w = (avg - ymin) / (ymax - ymin);
-
-//        //                if (lastNonZeroXi < 0)
-//        //                    lastNonZeroXi = xi-1;
-//        //                for (int xik=lastNonZeroXi+1; xik<=xi; xik++)
-//        //                    bins[xik] = w * 1000; // FIXME: 1000 is the resolution of the color scheme
-
-//        //                //print_debug ("sums[xi]: %f counts[xi]: %f ymin: %f, ymax: %f avg: %f => w: %f => bins[%d]: %d",
-//        //                //sums[xi], counts[xi], ymin, ymax, avg, w, xi, bins[xi]);
-//        //                sums[xi]   = 0.0;
-//        //                counts[xi] = 0.0;
-//        //                lastNonZeroXi = xi;
-//        //            }
-//        //        }
-//        //    }
-//        //    else
-//        //    {
-//        //        double x = xy[0];
-//        //        double y = xy[1];
-
-//        //        double s = canvas->world.scaleMtx[0][0];
-//        //        double xmin = canvas->world.centerPos[0] - s;
-//        //        double xmax = canvas->world.centerPos[0] + s;
-//        //        int xi = (x - xmin) / (xmax - xmin) * (w-1);
-
-//        //        if (xi >= 0 && xi < w)
-//        //        {
-//        //            sums[xi]   += y;
-//        //            counts[xi] += 1.0;
-//        //        }
-//        //    }
-//        //}
-//        else
-//        {
-//            exit_error ("unknown plot type '%c'", plotType);
-//        }
+//        print_warning ("truncating");
+//        i0 = 0;
 //    }
-//}
+//    if (isnan (xy[0]) || isnan (xy[1]))
+//    {
+//        // flush row
+//        for (uint32_t yi=h-1; yi>0; yi--)
+//            for (uint32_t xi=0; xi<w; xi++)
+//                bins[yi*w + xi] = bins[(yi-1) * w + xi];
+
+//        // construct new row
+//        int lastNonZeroXi = -1;
+//        for (uint32_t xi=0; xi<w; xi++)
+//        {
+//            if (canvas->counts[xi] > 1e-5)
+//            {
+//                double avg = sums[xi] / counts[xi];
+//                double s = canvas->world.scaleMtx[1][1];
+//                double ymin = canvas->world.centerPos[1] - s;
+//                double ymax = canvas->world.centerPos[1] + s;
+//                double w = (avg - ymin) / (ymax - ymin);
+
+//                if (lastNonZeroXi < 0)
+//                    lastNonZeroXi = xi-1;
+//                for (int xik=lastNonZeroXi+1; xik<=xi; xik++)
+//                    bins[xik] = w * 1000; // FIXME: 1000 is the resolution of the color scheme
+
+//                //print_debug ("sums[xi]: %f counts[xi]: %f ymin: %f, ymax: %f avg: %f => w: %f => bins[%d]: %d",
+//                //sums[xi], counts[xi], ymin, ymax, avg, w, xi, bins[xi]);
+//                sums[xi]   = 0.0;
+//                counts[xi] = 0.0;
+//                lastNonZeroXi = xi;
+//            }
+//        }
 
 void canvas_functions_register (void)
 {
@@ -459,6 +436,7 @@ void canvas_functions_register (void)
     cip_register_canvas_fun (2, '+', canvas_fun_2d_histogram_plus);
     cip_register_canvas_fun (2, 'l', canvas_fun_2d_line);
     cip_register_canvas_fun (2, 't', canvas_fun_2d_thick_line);
+    cip_register_canvas_fun (2, 's', canvas_fun_2d_stair);
     cip_register_canvas_fun (3, 'h', canvas_fun_3d_histogram);
     cip_register_canvas_fun (3, 'l', canvas_fun_3d_line);
 }
